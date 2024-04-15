393fe9852558416146845e06d30e732b21992717
Loui Dominic Naquita <louidominicnaquita16@gmail.com>
2024-04-15 21:53:28 +0800

    Added the LoginManager class for handling login

--- src/userAccountSystem/LoginManager.java
+++ src/userAccountSystem/LoginManager.java
@@ -4,9 +4,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import javax.swing.JFrame;
+
+import gui.windowFrames.WF_SuperAdminPanel;
 import sql.SQLConnector;
 
 public class LoginManager {
+
+    /**The current attempted access level that the LoginManager will use to
+     * facilitate login attempts based on the given access level.
+     */
+    private static int CURRENT_ACCESS_LEVEL = 0;
+
+    /**The current target JFrame of the attempted access level that the
+     * LoginManager will send in case of a successful login attempt of the
+     * given access level.
+     */
+    private static JFrame CURRENT_ACCESS_LEVEL_TARGET_JFRAME;
+
     public static boolean attemptSuperAdminLogin(String username, String password) throws SQLException {
         // Query setup
         final String query = "SELECT * FROM " + SQLConnector.sqlTbl + " WHERE access_level = 1 LIMIT 1";
@@ -21,9 +36,23 @@ public class LoginManager {
         if (SA_username.equals(username) && SA_password.equals(password)) {
             // Super admin access is permitted only when
             // the username and password matches credentials in DB
+            setCurrentAccessLevel(1, new WF_SuperAdminPanel());
             return true;
         } else {
             return false;
         }
+    }
+
+    private static void setCurrentAccessLevel(int accessLevel, JFrame targetJFrame) {
+        LoginManager.CURRENT_ACCESS_LEVEL = accessLevel;
+        LoginManager.CURRENT_ACCESS_LEVEL_TARGET_JFRAME = targetJFrame;
+    }
+
+    public static int getCurrentAccessLevel() {
+        return CURRENT_ACCESS_LEVEL;
+    }
+
+    public static JFrame getCurrentAccessLevelTargetJFrame() {
+        return CURRENT_ACCESS_LEVEL_TARGET_JFRAME;
     }
 }
