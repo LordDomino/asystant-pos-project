2df218c7097b3f26907cde722f9c7fa750fb0fb9
LordDomino <louidominicnaquita16@gmail.com>
2024-04-15 11:15:51 +0800

    Added WF_SuperAdminPanel class

---
+++ gui/windowFrames/WF_SuperAdminPanel.java
@@ -0,0 +1,19 @@
+package gui.windowFrames;
+
+import components.APP_Frame;
+
+public class WF_SuperAdminPanel extends APP_Frame {
+
+    public WF_SuperAdminPanel() {
+        super("Super Administrator Panel");
+        compile();
+    }
+
+    public void prepareComponents() {}
+    
+    public void prepare() {}
+
+    public void addComponents() {}
+
+    public void finalizePrepare() {}
+}
