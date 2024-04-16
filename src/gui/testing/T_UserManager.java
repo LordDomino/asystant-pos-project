61735672662b4b8c1b127773ccde15941b3c5728
LordDomino <louidominicnaquita16@gmail.com>
2024-04-16 10:50:49 +0800

    Added test file for WF_UserManager.java

--- src/gui/testing/T_UserManager.java
+++ src/gui/testing/T_UserManager.java
@@ -2,7 +2,7 @@ package gui.testing;
 
 import gui.windowFrames.WF_UserManager;
 
-public class T_SuperAdminScreen {
+public class T_UserManager {
     public static void main(String[] args) {
         WF_UserManager testFrame = new WF_UserManager();
         testFrame.setVisible(true);
