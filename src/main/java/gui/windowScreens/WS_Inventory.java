ae808a39136d1bde7dae14e6edfb0428d0c83bb7
LordDomino <louidominicnaquita16@gmail.com>
2024-04-22 11:09:55 +0800

    Added task for WS_Inventory

--- src/main/java/gui/windowScreens/WS_Inventory.java
+++ src/main/java/gui/windowScreens/WS_Inventory.java
@@ -1,10 +1,14 @@
 package main.java.gui.windowScreens;
 
 import javax.swing.JLabel;
+import javax.swing.JTabbedPane;
 
 import main.java.components.APP_Panel;
 
 public class WS_Inventory extends APP_Panel {
+
+    public final JLabel header = new JLabel("Inventory");
+    public final JTabbedPane tablesTabbedPane = new JTabbedPane();
 
     public JLabel test = new JLabel("Test");
 
@@ -15,11 +19,26 @@ public class WS_Inventory extends APP_Panel {
         add(test);
     }
 
-    public void prepare() {}
+    public void prepare() {
+        // background of entire Inventory view
+        // setBackground
+    }
 
-    public void prepareComponents() {}
+    public void prepareComponents() {
+        // set font of header here
+        // tablesTabbedPane.addTab ...
+        // when adding tabs, use new JPanel() for now
+    }
 
-    public void addComponents() {}
+    public void addComponents() {
+        // margins, insets, fill, anchor, etc
+        // use InsetsConfig for margins
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // header always has xxl margins for top and left
+        // jtabbedpane always has xxl margins for left and bottom,
+        // jtabbedpane m margins for top
+    }
 
     public void finalizePrepare() {}
 }
