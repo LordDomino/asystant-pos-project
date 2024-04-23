39924960ae5ff394b56cef9e771794445926734e
Potatopowers <118539229+Potatopowers@users.noreply.github.com>
2024-04-24 00:58:55 +0800

    changed method of JTable data.

--- src/main/java/gui/windowScreens/WS_StockTable.java
+++ src/main/java/gui/windowScreens/WS_StockTable.java
@@ -4,8 +4,13 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.util.ArrayList;
+
+import main.java.components.APP_AccentButton;
 import main.java.components.APP_Panel;
 
 public class WS_StockTable extends APP_Panel {
@@ -14,19 +19,43 @@ public class WS_StockTable extends APP_Panel {
     JPanel inventoryPanel = new JPanel();
     JPanel descriptionPanel = new JPanel();
 
-    String[] inventoryFields = {"Product Code", "Order Date", "Name", "Category", "Unit Cost", 
+    public static final String[] inventoryFields = {"Product Code", "Order Date", "Name", "Category", "Unit Cost", 
     "Stock Quantity", "Total Cost", "Stock Left"};
-
-    Object[][] placeholderObjects = {{"", "", "", "", "", "", "", ""}};
+    public static final ArrayList<ArrayList<String>> pendingDeletedProducts = new ArrayList<>();
 
     // @Potatopowers kaya mo gawin yung approach ng DefaultTableModel?
     // see WF_UserManager for reference
-    JTable inventoryTable = new JTable(placeholderObjects, inventoryFields);
+    DefaultTableModel inventoryModel = new DefaultTableModel(inventoryFields, 0);
+    
+    JTable inventoryTable = new JTable(inventoryModel);
     JScrollPane scrollPane = new JScrollPane(inventoryTable);
 
     JButton addButton = new JButton("Add");
-    JButton editButton = new JButton("Edit");
-    JButton deleteButton = new JButton("Delete");
+    JButton editButton = new JButton("Edit") {
+
+    public void fireValueChanged() {
+        int[] selectedIndices = inventoryTable.getSelectionModel().getSelectedIndices();
+
+        if (selectedIndices.length ==1) {
+            setEnabled(true);    
+        } else {
+            setEnabled(false);
+        }
+    }
+};
+    JButton deleteButton = new APP_AccentButton("Delete") {
+
+    public void fireValueChanged() {
+        int[] selectedIndices = inventoryTable.getSelectionModel().getSelectedIndices();
+
+        if (selectedIndices.length > 0) {
+            setEnabled(true);    
+        } else {
+            setEnabled(false);
+        }
+    }
+    };
+
 
     public WS_StockTable () {
         super();
@@ -34,6 +63,52 @@ public class WS_StockTable extends APP_Panel {
     }
 
     public void prepareComponents() {
+/* To be fixed.. 
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+            if (pendingDeletedProduct.size() >= 1) {
+                DeletePopUpWindow popUp = new DeletePopUpWindow();
+                popUp.info.setText(
+                    "<html>Adding new Products is not allowed while "
+                    + "some rows are pending to be deleted. "
+                    + "Confirm changes to pending deletions first."
+                ); 
+                popUp.setVisible(true);
+            } else {
+                AddPopupWindow popUp = new AddPopupWindow();
+                popUp.parentFrame = (WS_StockTable) SwingUtilities.getWindowAncestor(addButton);
+                popUp.setVisible(true);
+            }
+            }
+        });
+
+        editButton.addActionListener(new ActionListener()) {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                EditPopupWindow popUp = new EditpopUpWindow();
+                popUp.parentFrame = (WS_StockTable) SwingUtilities.getWindowAncestor(addButton);
+
+                if (table.getSelectionModel().getSelectedIndices().length != 1) {
+
+                } else {
+
+                    int rowIndex = inventoryTable.getSelectedRow();
+
+                    final String retrievedProductCode = inventoryTable.getValueAt(rowIndex, 0).toString();
+                    final String retrievedOrderDate = inventoryTable.getValueAt(rowIndex, 1).toString();
+                    final String retrievedName = inventoryTable.getValueAt(rowIndex, 2).toString();
+                    final String retrievedCategory = inventoryTable.getValueAt(rowIndex, 3).toString();
+                    final String retrievedUnitCost = inventoryTable.getValueAt(rowIndex, 4).toString();
+                    final String retrievedStockQuantity = inventoryTable.getValueAt(rowIndex, 5).toString();
+                    final String retrievedTotalCost = inventoryTable.getValueAt(rowIndex, 6).toString();
+                    final String retrievedStockLeft = inventoryTable.getValueAt(rowIndex, 7).toString();
+
+                    popUp.usernameEditField.setText(retrievedProductCode);
+                    popUp.user
+                }
+            }
+        }
+ */
 
     }
 
@@ -91,3 +166,5 @@ public class WS_StockTable extends APP_Panel {
 
     }
 }
+
+class AddPopupWindow extends APP_
