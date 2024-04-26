4812c35ea8c67481debd6b17a286af1c4c05e7fa
Potatopowers <118539229+Potatopowers@users.noreply.github.com>
2024-04-27 01:08:33 +0800

    finished asystant pos

--- src/main/resources/asystant_pos.sql
+++ src/main/resources/asystant_pos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2024 at 06:03 PM
+-- Generation Time: Apr 26, 2024 at 07:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -25,17 +25,21 @@ DELIMITER $$
 --
 -- Procedures
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `spRecomupteInventory` ()   update stocks_inventory_tbl ii
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spProcessOrderInventory` ()   update sales_tbl 
+set status = 2
+where status = 1$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `spRecomputeInventory` ()   update stocks_inventory_tbl ii
 
 JOIN 
 (
-    select sum(i.stock_quantity) - sum(p.quantity) qty_up  , i.product_code
+	select i.stock_quantity - sum(p.quantity) qty_up  , i.product_code
     from stocks_inventory_tbl i 
     inner join sales_tbl p ON i.product_code = p.item_code
     WHERE status = 1
     Group by i.product_code
 ) qq 
-on ii.item_code = qq.product_code
+on ii.product_code = qq.product_code
 set stock_quantity = qty_up$$
 
 DELIMITER ;
@@ -86,7 +90,7 @@ CREATE TABLE `sales_tbl` (
   `item_name` varchar(255) DEFAULT NULL,
   `quantity` int(12) DEFAULT NULL,
   `total_price` decimal(12,4) DEFAULT NULL,
-  `status` int(1) DEFAULT NULL COMMENT '0 = pending, 1 = completed',
+  `status` int(1) DEFAULT NULL COMMENT '0 = pending, 1 = completed, 2 = PROCESSED',
   `created_by` varchar(120) DEFAULT NULL,
   `created_datetime` datetime DEFAULT current_timestamp(),
   `completed_datetime` datetime DEFAULT NULL,
@@ -98,10 +102,10 @@ CREATE TABLE `sales_tbl` (
 --
 
 INSERT INTO `sales_tbl` (`sales_id`, `item_code`, `item_name`, `quantity`, `total_price`, `status`, `created_by`, `created_datetime`, `completed_datetime`, `customer_id`) VALUES
-('', 123456, 'Item1', 1, 102.0000, 1, 'user', '2024-04-26 23:47:57', NULL, NULL),
-(NULL, 123457, 'Item2', 1, 102.0000, 1, 'user', '2024-04-26 23:50:09', '2024-04-26 23:50:09', NULL),
-(NULL, 123457, 'Item2', 10, 2020.0000, NULL, 'user', '2024-04-26 23:51:11', '2024-04-26 23:51:11', NULL),
-(NULL, 123457, 'Item2', 1, 102.0000, NULL, 'user', '2024-04-26 23:51:28', '2024-04-26 23:51:28', NULL),
+('', 123456, 'Item1', 1, 102.0000, 2, 'user', '2024-04-26 23:47:57', NULL, NULL),
+(NULL, 123457, 'Item2', 1, 102.0000, 2, 'user', '2024-04-26 23:50:09', '2024-04-26 23:50:09', NULL),
+(NULL, 123457, 'Item2', 10, 2020.0000, 2, 'user', '2024-04-26 23:51:11', '2024-04-26 23:51:11', NULL),
+(NULL, 123457, 'Item2', 1, 102.0000, 2, 'user', '2024-04-26 23:51:28', '2024-04-26 23:51:28', NULL),
 (NULL, 123458, 'Item3', 1, 102.0000, NULL, 'user', '2024-04-26 23:51:38', '2024-04-26 23:51:38', NULL);
 
 -- --------------------------------------------------------
@@ -127,8 +131,8 @@ CREATE TABLE `stocks_inventory_tbl` (
 --
 
 INSERT INTO `stocks_inventory_tbl` (`id`, `product_code`, `name`, `description`, `category`, `unit_cost`, `stock_quantity`, `markup_price`, `unit_price`) VALUES
-(20, 123456, 'Item1', '', 'Uncategorized', 100.0000, 100, 2.0000, 102.0000),
-(21, 123457, 'Item2', '', 'Uncategorized', 100.0000, 100, 2.0000, 102.0000),
+(20, 123456, 'Item1', '', 'Uncategorized', 100.0000, 99, 2.0000, 102.0000),
+(21, 123457, 'Item2', '', 'Uncategorized', 100.0000, 88, 2.0000, 102.0000),
 (22, 123458, 'Item3', '', 'Uncategorized', 100.0000, 100, 2.0000, 102.0000);
 
 -- --------------------------------------------------------
